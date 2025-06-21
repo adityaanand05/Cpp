6d7f2d4e497e016237a9e27dcad0fc19784ad04b
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-06-21 20:47:06 +0530

    modified:   02_variables/variables.cpp

--- 02_variables/variables.cpp
+++ 02_variables/variables.cpp
@@ -4,14 +4,17 @@ int main() {
     // Declare and initialize variables
     int age = 25; // Integer variable
     double height = 5.9; // Double variable
+    float weight = 65.5f; // Float variable
     char initial = 'A'; // Character variable
     string name = "Aditya"; // String variable
-
+    string city;
     // Output the variables
     cout << "Name: " << name << endl;
     cout << "Initial: " << initial << endl;
     cout << "Age: " << age << endl;
     cout << "Height: " << height << endl;
-
+    cout << "Weight: " << weight << endl;
+    city = "New York"; // Assigning a value to the city variable
+    cout << "City: " << city << endl;
     return 0;
 }
