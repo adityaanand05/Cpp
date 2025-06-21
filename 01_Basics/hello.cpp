a144d71b099325d4e0113578b6e812da60b24872
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-06-21 20:03:51 +0530

    new file:   01_Basics/hello.cpp 	new file:   02_variables/variables.cpp 	deleted:    hello.cpp

---
+++ 01_Basics/hello.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// int main() {
+//     std::cout << "Hello, World!" << std::endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     cout << "Hello, World!" << endl;
+//     return 0;
+// }
+
+#include<iostream>//preprocessor directive
+using std::cout;
+using std::endl;
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ 02_variables/variables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // Declare and initialize variables
+    int age = 25; // Integer variable
+    double height = 5.9; // Double variable
+    char initial = 'A'; // Character variable
+    string name = "Aditya"; // String variable
+
+    // Output the variables
+    cout << "Name: " << name << endl;
+    cout << "Initial: " << initial << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+
+    return 0;
+}
