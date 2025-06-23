28d3f53bb751f856338caa82672c9861056332a3
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-06-23 18:48:04 +0530

    modified:   Assignment01/03.cpp 	new file:   Assignment01/04.cpp

--- Assignment01/03.cpp
+++ Assignment01/03.cpp
@@ -15,7 +15,26 @@
 
 using namespace std;
 
-int main() {
-    
+int main()
+{
+    int num1, num2, num3;
+    cout << "Enter yor first number :";
+    cin >> num1;
+    cout << "Enter yor second number :";
+    cin >> num2;
+    cout << "Enter yor third number :";
+    cin >> num3;
+    if (num1 >= num2 && num1 >= num3)
+    {
+        cout << num1 << " is greatest\n";
+    }
+    else if (num2 >= num1 && num2 >= num3)
+    {
+        cout << num2 << " is greatest\n";
+    }
+    else
+    {
+        cout << num3 << " is greatest\n";
+    }
     return 0;
 }
---
+++ Assignment01/04.cpp
@@ -0,0 +1,17 @@
+// Q3. Even or Odd Bitwise Style
+// Problem:
+// You’re writing firmware for a microcontroller. You need to determine if a number is even or odd using bitwise operators only.
+
+// Input:
+
+// int num
+
+// Condition:
+// Use & 1 to check the least significant bit.
+
+// Output:
+
+// "Even" or "Odd"
+
+// Focus: &, %, if-else
+
