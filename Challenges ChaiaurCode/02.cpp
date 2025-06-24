65dc27d6a5580af04daa3c5469d10ec00245eda3
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-06-24 11:12:32 +0530

    new file:   Challenges ChaiaurCode/02.cpp

---
+++ Challenges ChaiaurCode/02.cpp
@@ -0,0 +1,73 @@
+// Challenges for Students
+
+// Challenge 1: Arithmetic Operators
+// Write a program that calculates the price of tea packs. 
+//A user enters the number of tea packs they want, and the price per pack. Apply a 10% tax to the total price and display the final cost.
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int costprice_perpacket = 45 , number_of_packets , finalprice , totalprice; 
+    cout<<"Enter the number of packets : ";
+    cin >> number_of_packets;
+    totalprice = costprice_perpacket * number_of_packets;
+    finalprice = totalprice + totalprice*0.1;
+    cout << "Your final price with tax :" << finalprice;
+    return 0;
+}*/
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int costprice_perpacket  , number_of_packets , finalprice , totalprice; 
+    cout<<"Enter the cost of packets : ";
+    cin >> costprice_perpacket;
+    cout<<"Enter the number of packets : ";
+    cin >> number_of_packets;
+    totalprice = costprice_perpacket * number_of_packets;
+    finalprice = totalprice + totalprice*0.1;
+    cout << "Your final price with tax :" << finalprice;
+    return 0;
+}*/
+
+// Challenge 2: Assignment Operators
+// Create a program where the user inputs the number of tea bags they have. 
+//If the number is less than 20, give them 10 extra bags using the += assignment operator. Display the updated total.
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int no_of_teabags;
+    cout << "Enter your numbers of tea bags : ";
+    cin>> no_of_teabags;
+    if (no_of_teabags<20)
+    {
+        no_of_teabags += 10; 
+        cout << "hay, buddy you got 10 extra bags as you have bags less then 20. \n";
+        cout << "Now you have " << no_of_teabags<<" bags";
+    }
+    else
+    {
+            
+        cout << "you have " << no_of_teabags<<" bags";
+    }
+    
+    return 0;
+}*/
+
+// Challenge 3: Relational and Logical Operators
+// A tea subscription service offers a discount if the user buys more than 12 cups or if they are a member for more than a year. 
+//Write a program that checks if the user qualifies for a discount based on their input.
+
+// Challenge 4: Bitwise Operators
+// Write a program that uses bitwise AND (&) to check which tea types are in stock. The stock is encoded in an integer (1 for Green, 2 for Black, 4 for Oolong). 
+//Allow the user to check availability by inputting the tea type (1, 2, or 4), and display a message saying whether that tea is in stock or not.
