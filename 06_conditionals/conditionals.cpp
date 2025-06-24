86117b48d11af5e52eab11445cbd92b72dcf8881
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-06-24 16:53:42 +0530

    modified:   06_conditionals/conditionals.cpp

---
+++ 06_conditionals/conditionals.cpp
@@ -0,0 +1,37 @@
+// if (condition) {
+//     // Code block runs if condition is true
+// }
+
+
+
+// if (condition) {
+//     // Runs if condition is true
+// } else {
+//     // Runs if condition is false
+// }
+
+
+// if (condition1) {
+//     // Runs if condition1 is true
+// } else if (condition2) {
+//     // Runs if condition2 is true
+// } else {
+//     // Runs if none of the above conditions are true
+// }
+
+
+// int result = (condition) ? value_if_true : value_if_false;
+
+
+
+// int x = 2;
+// switch (x) {
+//     case 1:
+//         // code for case 1
+//         break;
+//     case 2:
+//         // code for case 2
+//         break;
+//     default:
+//         // code if no case matches
+// }
