898b629f29c08677d9a94f7c916322100d20f652
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-06-26 21:10:20 +0530

    modified:   Challenges ChaiaurCode/04.cpp

--- Challenges ChaiaurCode/04.cpp
+++ Challenges ChaiaurCode/04.cpp
@@ -3,43 +3,43 @@
 // Write a program that keeps asking the user to input the number of tea bags they have left. 
 //Stop when the number of bags is 0, and print a message that they are out of tea bags.
 
-// #include <iostream>
+#include <iostream>
 
-// using namespace std;
+using namespace std;
 
-// int main() {
-//     int numteabags;
-//     cout << "Enter Numbers of tea bag : ";
-//     cin >> numteabags;
-//     while (numteabags>0)
-//     {
-//         numteabags--;
-//         cout << "Enjoy your tea\n";
-//         cout << "You have " << numteabags <<" remaning\n";
-//     }
-//     cout << "Now, Ypu are out of bags";
-//     return 0;
-// }
+int main() {
+    int numteabags;
+    cout << "Enter Numbers of tea bag : ";
+    cin >> numteabags;
+    while (numteabags>0)
+    {
+        numteabags--;
+        cout << "Enjoy your tea\n";
+        cout << "You have " << numteabags <<" remaning\n";
+    }
+    cout << "Now, Ypu are out of bags";
+    return 0;
+}
 
 // Challenge 2: Do-While Loop
 // Write a program that asks the user if they want to add more sugar to their tea. Keep asking until they respond with "enough." Use a do-while loop for this.
 
-// #include <iostream>
-// #include <string>
-// using namespace std;
+#include <iostream>
+#include <string>
+using namespace std;
 
-// int main() {
-//     string response;
+int main() {
+    string response;
 
-//     do {
-//         cout << "Do you want to add more sugar to your tea? ";
-//         getline(cin, response);
-//     } while (response != "enough");
+    do {
+        cout << "Do you want to add more sugar to your tea? ";
+        getline(cin, response);
+    } while (response != "enough");
 
-//     cout << "Alright, your tea is sweet enough now!\n";
+    cout << "Alright, your tea is sweet enough now!\n";
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 // Challenge 3: For Loop
@@ -81,5 +81,61 @@ int main() {
 // Write a program that checks a list of tea types and serves tea for each type, except "Herbal Tea." If "Herbal Tea" is encountered, skip it using continue. 
 //If "No Tea" is encountered, stop the loop using break.
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string teatypes[] = {
+        "Green Tea", "Black Tea", "Herbal Tea", "Oolong Tea", "No Tea", "White Tea"
+    };
+
+    int size = sizeof(teatypes) / sizeof(teatypes[0]);
+
+    for (int i = 0; i < size; ++i) {
+        if (teatypes[i] == "Herbal Tea") {
+            cout << "Skipping Herbal Tea.\n";
+            continue;
+        }
+
+        if (teatypes[i] == "No Tea") {
+            cout << "No Tea found. Stopping service.\n";
+            break;
+        }
+
+        cout << "Serving " << teatypes[i] << endl;
+    }
+
+    return 0;
+}
+
 // Challenge 5: Nested Loops
-// Write a program that displays a tea brewing schedule. For each day of the week (outer loop), brew 2 cups of tea for breakfast, lunch, and dinner (inner loop).
+// Write a program that displays a tea brewing schedule. 
+//For each day of the week (outer loop), brew 2 cups of tea for breakfast, lunch, and dinner (inner loop).
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int days=7;
+    string foodtime[3]={
+        "breakfast", "lunch", "dinner"
+    };
+    for (int i = 1; i <= days; i++)
+    {
+        cout << "Day "<<i<< endl;
+        for (int j = 0; j < 3 ; j++)
+        {
+           cout << "Brewing cups of tea for " <<foodtime[j]<<endl;
+           for (int t = 1; t<=2;t++)
+           {
+                cout << "Brewin "<<t<<" cups of tea\n";
+           }
+           
+        }
+        
+    }
+    
+    return 0;
+}
