7a9b97bb545f7e95c0d7e36ed24064189beb3b87
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-06-27 20:47:17 +0530

    modified:   07_Function/function.cpp

--- 07_Function/function.cpp
+++ 07_Function/function.cpp
@@ -1,26 +1,30 @@
-#include<iostream>
+#include <iostream>
 #include <string>
 using namespace std;
-//normal function 
-int sum(int a, int b){
+
+// normal function
+int sum(int a, int b)
+{
     int add;
-    add = a+b;
+    add = a + b;
     cout << "Sum = " << add;
     return add;
 }
-//recursion
-int factorial(int n) {
-    if (n <= 1) return 1;            // Base case
-    return n * factorial(n - 1);     // Recursive case
+
+// recursion
+int factorial(int n)
+{
+    if (n <= 1)
+        return 1;                // Base case
+    return n * factorial(n - 1); // Recursive case
 }
-
-
 
 // Function overloading
 void car(int);
 void car(string);
 
-int main(){
+int main()
+{
     // int i,j;
     // cout << "Enter Your first number :";
     // cin>>i;
@@ -30,24 +34,22 @@ int main(){
     // sum(i,j);
     // car();
 
-
-// This a lambda a fuction made and called for specific(once called or not needed to call many times) code 
-    auto tea = [](int ){
+    // This a lambda a fuction made and called for specific(once called or not needed to call many times) code
+    auto tea = [](int)
+    {
         int lovefortea;
         cout << "How much do you love tea (tell in %): ";
         cin >> lovefortea;
-        if (lovefortea<80)
+        if (lovefortea < 80)
         {
             cout << "Dam man you don't like tea.";
-        }else{
+        }
+        else
+        {
             cout << "Me too bro";
         }
-        
-
     };
     tea(81);
-
-
 
     // int num;
     // cout << "Enter a number: ";
@@ -58,17 +60,18 @@ int main(){
 
 // Function overloading - When two function is named with same name.
 
-void car(int){
+void car(int)
+{
     int totalcar; // this allow user to take input in function
     cout << "Enter total numbedr of cars :";
     cin >> totalcar;
     cout << "Total cars =" << totalcar;
 }
 
-void car(string){
+void car(string)
+{
     string carname;
     cout << "Enter name of car : ";
     getline(cin, carname);
-    cout << "You chossed "<<carname;
-
+    cout << "You chossed " << carname;
 }
