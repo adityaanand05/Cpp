fb13e23a2d8d927a103d36c37d32b8077b533fd4
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-07-01 18:25:31 +0530

    modified:   08_Array/pointer.cpp

--- 08_Array/pointer.cpp
+++ 08_Array/pointer.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+    /*
     int a = 55;
     int* ptr = &a; // normal pointer storing address of a variable
     int** ptr1 = &ptr; //pointer to pointer 'ek pointer me dusra pointer'
@@ -13,7 +14,7 @@ int main() {
     cout << "no pointer direct addr of a using & : "<<&a <<endl<<endl;
     cout << "Value of pointer(ptr1): "<<ptr1 <<endl<<endl;
     cout <<"no pointer direct addr of ptr using & : "<< &ptr <<endl<<endl;
-
+    */
 
     /*
         * is dereference(value at address) operator for case 
@@ -49,6 +50,25 @@ int main() {
         4. p-=2 or p = p-2
         5. d = q-p
     */
+
+    /*
+        References
+        int a = 20;
+        int &y = a ;
+
+        cout << a << endl;
+        a++;
+        y++;
+        cout << a << endl;
+        cout<<&y<<endl;
+        cout<<&a<<endl;
+
+    
+    */
+        int A[5]={7,8,9,12,5};
+        int* p=A;
+
+        cout << p <<endl;
 
     return 0;
 }
