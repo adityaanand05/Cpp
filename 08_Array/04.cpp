553934b5d066abb592056d5cc38d3b108f9d298f
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-07-08 09:32:25 -0400

    1st commit

--- 08_Array/04.cpp
+++ 08_Array/04.cpp
@@ -1,22 +1,45 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    int Age[3][3] = {
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
-    };
-    for (int i = 0; i < 3; i++)
-    {
-        cout << "\nAge Group :"<<i+1<<endl;
-        for (int j = 0; j < 3; j++)
-        {
-            cout << Age[i][j] << "\t";
-        }
+// int main() {
+//     int Age[3][3] = {
+//         {1,2,3},
+//         {4,5,6},
+//         {7,8,9}
+//     };
+//     for (int i = 0; i < 3; i++)
+//     {
+//         cout << "\nAge Group :"<<i+1<<endl;
+//         for (int j = 0; j < 3; j++)
+//         {
+//             cout << Age[i][j] << "\t";
+//         }
         
-    }
+//     }
     
-    return 0;
+//     return 0;
+// }
+
+//Example using std::vector:
+#include <iostream>
+#include <vector> // Include the vector header
+int main() {
+std::vector<int> numbers = {10, 20, 30, 40, 50}; // Initialize a vector
+// Add elements
+numbers.push_back(60);
+numbers.push_back(70);
+// Access elements
+std::cout << "Element at index 0: " << numbers[0] << std::endl;
+std::cout << "Element at index 2 (using at()): " << numbers.at(2)
+<< std::endl;
+// Iterate
+std::cout << "Vector elements: ";
+for (int num : numbers) {
+std::cout << num << " ";
+}
+std::cout << std::endl;
+// Get size
+std::cout << "Vector size: " << numbers.size() << std::endl;
+return 0;
 }
